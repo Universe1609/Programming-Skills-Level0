622284720717f29929958524e807a3d6660de3b4
universe <davidrolandoaguirre@gmail.com>
2024-03-29 10:59:34 -0500

    first commit challenge_0

---
+++ challenge_1/online_banking_system.py
@@ -0,0 +1,39 @@
+#1. Create an online banking system with the following features:
+
+#* Users must be able to log in with a username and password.
+#* If the user enters the wrong credentials three times, the system must lock them out.
+#* The initial balance in the bank account is $2000.
+#* The system must allow users to deposit, withdraw, view, and transfer money.
+#* The system must display a menu for users to perform transactions.
+from passlib.context import CryptContext
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class log_in():
+    
+    def __init__(self, username, password) -> None:
+        self.username = username
+        self.password = password
+        self.hashed = False
+        self.balance = 2000
+        self.login_attempts = 0
+        
+    def set_security_password(self, password) -> None:
+        self.password = pwd_context.hash(password)
+        
+    def verify_password(self, password) -> bool:
+        if pwd_context.verify(password, self.password):
+            self.login_attempts = 0
+            return True
+        else:
+            self.login_attempts += 1
+            if self.login_attempts >= 3:
+                return False #after this, i have to locked it, creating self.locked 
+            
+        return pwd_context.verify(password, self.password)
+    
+    
+    def wrong_credentials(self, credential) -> None:
+        #not needed cause already implemented in verify_password
+        pass
