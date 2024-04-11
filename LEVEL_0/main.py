3c89ea062fc06d5dd20c3da9f44bc5273c7ff19f
universe <davidrolandoaguirre@gmail.com>
2024-04-10 20:46:15 -0500

    correct strings values to integers in programs

--- LEVEL_0/main.py
+++ LEVEL_0/main.py
@@ -1,12 +1,12 @@
 #3. Create an university enrollment system with the following characteristics:
-#* 		The system has a login with a username and password.
-#* 		Upon logging in, a menu displays the available programs: Computer Science, Medicine, Marketing, and Arts.
-#* 		The user must input their first name, last name, and chosen program.
-#* 		Each program has only 5 available slots. The system will store the data of each registered user, 
-#       and if it exceeds the limit, it should display a message indicating the program is unavailable.
-#* 		If login information is incorrect three times, the system should be locked.
-#* 		The user must choose a campus from three cities: London, Manchester, Liverpool.
-#* 		In London, there is 1 slot per program; in Manchester, there are 3 slots per program, and in Liverpool, there is 1 slot per program.
+#* 	(X)	The system has a login with a username and password.
+#* 	(X)	Upon logging in, a menu displays the available programs: Computer Science, Medicine, Marketing, and Arts.
+#* 	(X)	The user must input their first name, last name, and chosen program.
+#* 	( )	Each program has only 5 available slots. The system will store the data of each registered user, 
+# and if it exceeds the limit, it should display a message indicating the program is unavailable.
+#* 	(X)	If login information is incorrect three times, the system should be locked.
+#* 	( )	The user must choose a campus from three cities: London, Manchester, Liverpool.
+#* 	( )	In London, there is 1 slot per program; in Manchester, there are 3 slots per program, and in Liverpool, there is 1 slot per program.
 #* 		If the user selects a program at a campus that has no available slots, the system should display the option to enroll in the program at another campus.
 from passlib.context import CryptContext
 
@@ -65,29 +65,29 @@ while True:
     accounts = {}
     
     programs = {
-        'Computer Science': {'London': {'slots': '1',
+        'Computer Science': {'London': {'slots': 1,
                                         'enroll': []},
-                             'Manchester': {'slots': '3',
+                             'Manchester': {'slots': 3,
                                             'enroll': []},
-                             'Liverpool': {'slots': '3',
+                             'Liverpool': {'slots': 3,
                                            'enroll': []}},
-        'Medicine': {'London': {'slots': '1',
+        'Medicine': {'London': {'slots': 1,
                                 'enroll': []},
-                    'Manchester': {'slots': '3',
+                    'Manchester': {'slots': 3,
                                     'enroll': []},
-                    'Liverpool': {'slots': '3',
+                    'Liverpool': {'slots': 3,
                                    'enroll': []}},
-        'Marketing': {'London': {'slots': '1',
+        'Marketing': {'London': {'slots': 1,
                                  'enroll': []},
-                      'Manchester': {'slots': '3',
+                      'Manchester': {'slots': 3,
                                      'enroll': []},
-                      'Liverpool': {'slots': '3',
+                      'Liverpool': {'slots': 3,
                                     'enroll': []}},
-        'Arts': {'London': {'slots': '1',
+        'Arts': {'London': {'slots': 1,
                             'enroll': []},
-                 'Manchester': {'slots': '3',
+                 'Manchester': {'slots': 3,
                                 'enroll': []},
-                 'Liverpool': {'slots': '3',
+                 'Liverpool': {'slots': 3,
                                'enroll': []}}
         }
     
